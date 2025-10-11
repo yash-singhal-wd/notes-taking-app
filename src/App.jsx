fef9e9f5acf2323f904f25b062ec6eac729dda7b
ZafarWD <yash98singhal@gmail.com>
2025-10-11 16:50:44 +0530

    optional feature list added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 // make 3 main screens - collapsible sidenavbar, a normal navbar and main pages of notes
 // SideNavbar, Navbar, MainScreen
-// localstorage feature - useEffect
+// localstorage feature - useEffect - for persistence
 // ContextAPI and useReducer
+
+// Optional:
+//Framer Motion for subtle animations
+//Add Markdown preview mode
+//Add a search bar to filter notes
+//Add a “recently edited” list
 
 export default function App() {
   return <div>Hello Notes app here</div>;
