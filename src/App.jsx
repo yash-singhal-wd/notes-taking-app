61532f747d6d22e5e535aac4dc7ac97253c6cba3
ZafarWD <yash98singhal@gmail.com>
2025-10-11 16:53:02 +0530

    optional feature list added

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import React from "react";
 // localstorage feature - useEffect - for persistence
 // ContextAPI and useReducer
 
+//Features:
+//Theme feature
+//Sidebar Management
+//CRUD operations on Notes
+
 // Optional:
 //Framer Motion for subtle animations
 //Add Markdown preview mode
 //Add a search bar to filter notes
 //Add a “recently edited” list
+//
 
 export default function App() {
   return <div>Hello Notes app here</div>;
