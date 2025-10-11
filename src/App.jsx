95698a3dc23b25a9f5e56a11392cf12117f871dd
ZafarWD <yash98singhal@gmail.com>
2025-10-11 16:45:32 +0530

    setting the project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+// make 3 main screens - collapsible sidenavbar, a normal navbar and main pages of notes
+// SideNavbar, Navbar, MainScreen
+//
+
 export default function App() {
   return <div>Hello Notes app here</div>;
 }
