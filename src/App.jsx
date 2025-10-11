7ba037a0214cd4bcfd2428588dc7a64cf13afece
ZafarWD <yash98singhal@gmail.com>
2025-10-11 16:47:59 +0530

    setting the features

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 // make 3 main screens - collapsible sidenavbar, a normal navbar and main pages of notes
 // SideNavbar, Navbar, MainScreen
-//
+// localstorage feature - useEffect
+// ContextAPI and useReducer
 
 export default function App() {
   return <div>Hello Notes app here</div>;
